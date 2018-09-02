04de8bd1ed29f66fbbd5019fddfbd96b88e8d9b5
Dennis Schulmeister-Zimolong <dennis@patk.org>
2018-09-02 18:04:29 +0200

    Initial code import
    This imports all the code from the dhbwka-wwi-atom-github-beispiel
    tutorial except for the HTML content. Comments have been translated to
    English and a README.md as well as a LICENSE file have been added.

---
+++ README.md
@@ -0,0 +1,43 @@
+mini-tutorial.js - Beautiful HTML tutorials with minimal effort
+===============================================================
+
+Documentation
+-------------
+
+ * See mini-tutorial.js in action: [Manual](https://www.wpvs.de/mini-tutorial/)
+ * Download the [manual source code](https://www.github.com/DennisSchulmeister/mt-manual)
+ * Download the [template project](https://www.github.com/DennisSchulmeister/mt-template) to get started
+
+What is it
+----------
+
+Oh, nothing special but thanks for asking. Basically it is just a tiny hack to
+solve a problem of mine. For my lectures I wanted to be able to put tutorials
+online without much effort. Tiny manuals or documentation pages with a clean
+and beautiful look and a possibility to browse between chapters. This is,
+instead of simply showing a long HTML page, it should be possible to only show
+one chapter at a time and read through them like a book.
+
+So here it is. One the result of one evening of coding. A minimal single page
+app which allows to easily build beautiful HTML tutorials. No feature bloat
+and no external dependencies. Just one VanillaJS class and a stylesheet.
+And it doesn't get in your way, except for the stylesheet which applies global
+styling to your document.
+
+Screenshots
+-----------
+
+Everybody loves screenshots, right? Well I do, so feel right at home:
+
+![Screenshot](screenshot.png)
+
+Copyright
+---------
+
+mini-tutorial.js (https://www.github.com/DennisSchulmeister/mini-tutorial.js) <br/>
+© 2018 Dennis Schulmeister-Zimolong <dennis@pingu-mail.de>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
---
+++ mini-tutorial.js
@@ -0,0 +1,290 @@
+/*
+ * mini-tutorial.js (https://www.wpvs.de/mini-tutorial/)
+ * © 2018  Dennis Schulmeister-Zimolong <dennis@pingu-mail.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ */
+"use strict";
+
+/**
+ * This tiny class controls our web application.
+ */
+export class MiniTutorial {
+    /**
+     * Yeah! The construktor.
+     */
+    constructor() {
+        this.body = document.querySelector("body");
+        this.sections = document.querySelectorAll("section");
+        this.nav = document.querySelector("nav");
+
+        this.index = 0;
+        this.titlePrefix = document.title;
+
+        window.addEventListener("click", event => this._onLinkClicked(event));
+        window.addEventListener("popstate", event => this._onHistoryChanged(event));
+        window.addEventListener("keyup", event => this._handleKeyUpEvent(event));
+    }
+
+    /**
+     * Call this method in a window load event handler, in order to start
+     * the application. Like this:
+     *
+     *   window.addEventListener("load", () => {
+     *       let mt = new MiniTutorial();
+     *       mt.start();
+     *   });
+     */
+    start() {
+        this._hideAllSections();
+        this._insertHeadings();
+        this._buildTOC();
+
+        let index = location.hash.slice(1);
+        index = parseInt(index);
+
+        if (isNaN(index)) index = 1;
+        this._showSection(index);
+    }
+
+    /**
+     * Hide all <section> except the one with id="toc", which is the
+     * Table of Contents. This simply adds the CSS class "hidden" to
+     * each <section>.
+     */
+    _hideAllSections() {
+        this.sections.forEach(section => {
+            if (section.id === "toc") return;
+            section.classList.add("hidden");
+        });
+    }
+
+    /**
+     * Haide all <section> and show the one with the given index, instead.
+     * The index always starts at 1, since index 0 is the Table of Contents,
+     * which should always be visible.
+     *
+     * @param {int} index <section> to be shown, starting at 1
+     */
+    _showSection(index) {
+        // Check index
+        if (index < 1 || index >= this.sections.length) index = 1;
+
+        // Push new entry to the browser's navigation history
+        this._pushNavigationHistory(index, this.index);
+        this.index = index;
+
+        // Show requested <section>
+        this._hideAllSections();
+        let section = this.sections[index];
+
+        if (section === undefined) return;
+        section.classList.remove("hidden");
+
+        // Reset window scroll bars
+        window.scrollTo(0, 0);
+
+        // Update window title
+        if (section.dataset.title) {
+            document.title = `${this.titlePrefix} – ${section.dataset.title}`;
+        } else {
+            document.title = this.titlePrefix;
+        }
+
+        // Highligh current <section> in the Table of Contents
+        let currentIndex = 0;
+
+        document.querySelectorAll("#toc li a").forEach(tocItem => {
+            currentIndex++;
+
+            if (currentIndex === index) {
+                tocItem.classList.add("active");
+            } else {
+                tocItem.classList.remove("active");
+            }
+        });
+
+        // Update navigation links
+        this.nav.innerHTML = "";
+        let link_prev = document.createElement("a");
+        let link_next = document.createElement("a");
+
+        this.nav.appendChild(link_prev);
+        this.nav.appendChild(link_next);
+
+        if (index > 1) {
+            let sectionPrev = this.sections[index - 1];
+
+            if (sectionPrev.dataset.title) {
+                link_prev.textContent = sectionPrev.dataset.title;
+                link_prev.href = "#" + (index - 1);
+            }
+        }
+
+        if (index < this.sections.length - 1) {
+            let sectionNext = this.sections[index + 1];
+
+            if (sectionNext.dataset.title) {
+                link_next.textContent = sectionNext.dataset.title;
+                link_next.href = "#" + (index + 1);
+            }
+        }
+
+        // Show <body> in case it is still invisbile. This prevents flickering
+        // all <section> at the initial page load.
+        this.body.classList.remove("hidden");
+    }
+
+    /**
+     * Place an <h2> heading at the beginning of each <section>, except for
+     * the Table of Contents, which gets an <h3> heading. The heading is taken
+     * from the data-title attribute of each <section>.
+     */
+    _insertHeadings() {
+        this.sections.forEach(section => {
+            let title = section.dataset.title;
+            if (title === undefined) return;
+
+            let headingType = "h2";
+
+            if (section.id === "toc") headingType = "h3";
+
+            let heading = document.createElement(headingType);
+            heading.textContent = title;
+            section.insertBefore(heading, section.childNodes[0]);
+        });
+    }
+
+    /**
+     * Build Table of Contents
+     */
+    _buildTOC() {
+        let list = document.createElement("ol");
+        let index = -1;
+
+        this.sections.forEach(section => {
+            index++;
+
+            if (section.id === "toc") {
+                section.innerHTML = "";
+                section.appendChild(list);
+                return;
+            }
+
+            let title = section.dataset.title;
+            if (title === undefined) return;
+
+            let link = document.createElement("a");
+            link.textContent = title;
+            link.href = "#" + index;
+
+            let listItem = document.createElement("li");
+            listItem.appendChild(link);
+            list.appendChild(listItem);
+        });
+    }
+
+    /**
+     * Event handler for clicked <a> elements. This switches the currently
+     * visible <section> if the link starts with a hash-tag. e.g. #1 or #42.
+     * All other links will be ignored and work as normal.
+     *
+     * @param {DOMEvent} event Captured click event
+     */
+    _onLinkClicked(event) {
+        let target = event.target;
+        while (target && target.nodeName != "A") target = target.parentNode;
+        if (!target || target.nodeName != "A") return;
+
+        let href = target.getAttribute("href");
+        if (href === null || !href.startsWith("#")) return;
+
+        let index = target.hash.slice(1);
+        if (!index.length) return;
+
+        index = parseInt(index);
+        if (index === NaN) return;
+
+        event.preventDefault();
+        this.showSection(index);
+    }
+
+    /**
+     * Switch visible <section> when the user presses the browser's back
+     * button. This is working, since the methode this._pushNavigationHistory()
+     * is called, as soon as the visible <section> is switched.
+     *
+     * @param {DOMEvent} event Captured popstate event
+     */
+    _onHistoryChanged(event) {
+        let index = 1;
+
+        if (event.state) {
+            let state = JSON.parse(event.state)
+            index = state.index;
+        } else {
+            index = location.hash.slice(1);
+        }
+
+        index = parseInt(index);
+        if (isNaN(index)) return;
+
+        this._lockHistory = true;
+        this.showSection(index);
+        this._lockHistory = false;
+    }
+
+    /**
+     * Push new entry to the browser's navigation history, once another
+     * <section> is shown. This method is called by this._showSection().
+     *
+     * @param  {Integer} newIndex Index of the new <section>, starting at 1
+     * @param  {Integer} oldIndex Previous index or 0 if none
+     */
+    _pushNavigationHistory(newIndex, oldIndex) {
+        if (this._lockHistory) return;
+
+        let state = {
+            index: newIndex,
+        };
+
+        let url = `#${newIndex}`;
+
+        if (oldIndex == 0) {
+            history.replaceState(JSON.stringify(state), "", url);
+        } else {
+            history.pushState(JSON.stringify(state), "", url);
+        }
+    }
+
+    /**
+     * Keyboard navigation. Switch visible <section> with Arrow Left, Arrow
+     * Right, Space and Enter.
+
+     * @param {DOMEvent} event Captured keyup event
+     */
+    _handleKeyUpEvent(event) {
+        if (event.ctrlKey || event.shiftKey || event.altKey || event.metaKey) return;
+
+        switch (event.code) {
+            case "ArrowLeft":
+                // Previous <section>
+                if (this.index > 1) {
+                    this.showSection(this.index - 1);
+                }
+                break;
+            case "ArrowRight":
+            case "Enter":
+            case "Space":
+            case "KeyN":
+                // Next <section>
+                if (this.index < this.sections.length - 1) {
+                    this.showSection(this.index + 1);
+                }
+                break;
+        }
+    }
+}
